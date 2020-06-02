1fe12dc2a9eac333361a87af223d3504484513d7
Richard Carino <richard.carino17@gmail.com>
2020-06-03 00:23:14 +1000

    Add project files.

---
+++ 2.6 Studio/Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2._6_Studio
+{
+    class Circle
+    {
+        const double pi = 3.14;
+        public double radius { get; set; }
+        public double area;
+
+        public void GetArea()
+        {
+            area = pi * radius * radius;
+        }
+    }
+}
---
+++ 2.6 Studio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace _2._6_Studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var circle = new Circle();
+
+            Console.WriteLine("Enter a radius:");
+
+            circle.radius = CheckIfNum.CheckNum(Console.ReadLine());
+            circle.GetArea();
+            
+
+            Console.WriteLine("The area of the circle is {0}", circle.area);
+
+        }
+    }
+}
